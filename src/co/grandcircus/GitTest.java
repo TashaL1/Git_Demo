794efbb9b5d916a3ed5bd133b134c50173f5cae7
Natasha Langston <langstonnatasha@gmail.com>
2019-07-16 11:27:17 -0400

    added name code

--- src/co/grandcircus/GitTest.java
+++ src/co/grandcircus/GitTest.java
@@ -1,10 +1,20 @@
 package co.grandcircus;
 
+import java.util.Scanner;
+
 public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		int age = 21;
+		System.out.println(age);
 
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = scan.nextLine();
+		System.out.println(name);
 	}
 
 }
